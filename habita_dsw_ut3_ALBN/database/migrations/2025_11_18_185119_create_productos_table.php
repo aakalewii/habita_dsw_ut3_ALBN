5e06a94e2a34a823ddb5cfeacf516b16496b7ad1
alvaro123124 <alvarobudy12@gmail.com>
2025-11-18 19:32:06 +0000

    migraciones de categorai y productos

--- habita_dsw_ut3_ALBN/database/migrations/2025_11_18_185119_create_productos_table.php
+++ habita_dsw_ut3_ALBN/database/migrations/2025_11_18_185119_create_productos_table.php
@@ -4,22 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('productos', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->decimal('precio', 8, 2);
+            $table->integer('stock')->default(0);
+            $table->string('materiales');
+            $table->string('dimensiones');
+            $table->string('color_principal');
+            $table->string('imagen_principal')->nullable();
+            $table->boolean('destacado')->default(false);
             $table->timestamps();
+
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('set null');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('productos');
