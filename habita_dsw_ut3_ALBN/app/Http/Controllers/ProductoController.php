6baa9240413900ebc0c1b3e787f195755aca7368
alvaro123124 <alvarobudy12@gmail.com>
2025-11-24 16:55:38 +0000

    Filtros

--- habita_dsw_ut3_ALBN/app/Http/Controllers/ProductoController.php
+++ habita_dsw_ut3_ALBN/app/Http/Controllers/ProductoController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class ProductoController extends Controller
 {
-    //
+    public function galeria(Request $request)
+    {
+        // Trae los productos y las categorías en una única consulta
+        $query = Producto::with('categoria');
+
+        // Búsqueda por nombre
+        // filled('buscar') verifica si el campo de búsqueda no está vacío.
+        // where('nombre', 'like', '%texto%') Filtra productos cuyo nombre contenga el texto introducido.
+        if ($request->filled('buscar')) {
+            $query->where('nombre', 'like', '%' . $request->buscar . '%');
+        }
+
+        //Busqueda por descripcion
+        if ($request->filled('buscar')) {
+            $query->where('descripcion', 'like', '%' . $request->descripcion . '%');
+        }
+
+        //Rango precio
+        
+
+        // Filtro por categoría
+        // Si el usuario selecciona una categoría, se añade un filtro a la consulta.
+        // Solo se mostrarán los productos que pertenezcan a esa categoría.
+        if ($request->filled('categoria_id')) {
+            $query->where('categoria_id', $request->categoria_id);
+        }
+
+        //Filtro por color
+        if ($request->filled('color_principal')) {
+            $query->where('colo_principal', $request->color_principal);
+        }
+
+        // Ejecuta la consulta final con todos los filtros aplicados.
+        // Divide el resultado en páginas de 12 productos por página.
+        $productos = $query->paginate(12);
+        $categorias = Categoria::all();
+
+        return view('productos.galeria', compact('productos', 'categorias'));
+    }
 }
