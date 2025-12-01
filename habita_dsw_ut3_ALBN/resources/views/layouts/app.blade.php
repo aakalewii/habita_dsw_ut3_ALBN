5d0da406552c25111c8fe50b875caa02513a8db8
aakalewii <erlewiirobot@gmail.com>
2025-12-01 19:26:10 +0000

    estilos de bootstrap distintos

--- habita_dsw_ut3_ALBN/resources/views/layouts/app.blade.php
+++ habita_dsw_ut3_ALBN/resources/views/layouts/app.blade.php
@@ -7,8 +7,7 @@
     <title>@yield('title', 'Autenticación - Gestión de Productos')</title>
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/lux/bootstrap.min.css" rel="stylesheet">
 
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
