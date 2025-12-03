806da515a1bcb5e67d088bb72a4fc4f82a09713e
alvaro123124 <alvarobudy12@gmail.com>
2025-12-03 15:58:16 +0000

    boton

--- habita_dsw_ut3_ALBN/resources/views/layouts/menu.blade.php
+++ habita_dsw_ut3_ALBN/resources/views/layouts/menu.blade.php
@@ -7,18 +7,6 @@
             </a>
         </li>
 
-        <!-- Botón de Desplegable para Móviles -->
-        <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
         <!-- Opciones del Menú -->
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
